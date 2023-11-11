03b368bdd5830dc258c20a8a9d694f51341331bf
Caiottk <caionarutolegal@gmail.com>
2023-11-10 21:26:39 -0300

    Conflitos resolvidos e primeira implementação do motor para testar

--- Lab03/Project/gpio.c
+++ Lab03/Project/gpio.c
@@ -12,6 +12,8 @@
 #define GPIO_PORTN  (0x1000) //bit 12
 #define GPIO_PORTF  (0x0020) // bit 5
 #define GPIO_PORTA  (0x0001) // bit 0
+#define GPIO_PORTQ  (0x4000) // bit 14
+#define GPIO_PORTP  (0x2000) // bit 13
 
 extern void timerInit(void);
 
@@ -23,40 +25,50 @@ extern void timerInit(void);
 void GPIO_Init(void)
 {
 	//1a. Ativar o clock para a porta setando o bit correspondente no registrador RCGCGPIO
-	SYSCTL_RCGCGPIO_R = (GPIO_PORTF | GPIO_PORTJ | GPIO_PORTN | GPIO_PORTA);
+	SYSCTL_RCGCGPIO_R = (GPIO_PORTF | GPIO_PORTJ | GPIO_PORTN | GPIO_PORTA | GPIO_PORTQ | GPIO_PORTP);
 	//1b.   apos isso verificar no PRGPIO se a porta esta pronta para uso.
-   while((SYSCTL_PRGPIO_R & (GPIO_PORTF | GPIO_PORTJ | GPIO_PORTN | GPIO_PORTA) ) !=
-	      					(GPIO_PORTF | GPIO_PORTJ | GPIO_PORTN | GPIO_PORTA)){ };
+   while((SYSCTL_PRGPIO_R & (GPIO_PORTF | GPIO_PORTJ | GPIO_PORTN | GPIO_PORTA | GPIO_PORTQ | GPIO_PORTP) ) !=
+	      					(GPIO_PORTF | GPIO_PORTJ | GPIO_PORTN | GPIO_PORTA | GPIO_PORTQ | GPIO_PORTP)){ };
 	
 	// 2. Limpar o AMSEL para desabilitar a analogica
 	GPIO_PORTA_AHB_AMSEL_R = 0x00;
 	GPIO_PORTJ_AHB_AMSEL_R = 0x00;
 	GPIO_PORTN_AMSEL_R = 0x00;
 	GPIO_PORTF_AHB_AMSEL_R = 0x00;
+	GPIO_PORTQ_AMSEL_R = 0x00;
+	GPIO_PORTP_AMSEL_R = 0x00;
 	
 	// 3. Limpar PCTL para selecionar o GPIO
 	GPIO_PORTA_AHB_PCTL_R = 0x11;
 	GPIO_PORTJ_AHB_PCTL_R = 0x00;
 	GPIO_PORTN_PCTL_R = 0x00;
 	GPIO_PORTF_AHB_PCTL_R = 0x00;
+	GPIO_PORTQ_PCTL_R = 0x00;
+	GPIO_PORTP_PCTL_R = 0x00;
 
 	// 4. DIR para 0 se for entrada, 1 se for saida
 	GPIO_PORTA_AHB_DIR_R = 0x02;
 	GPIO_PORTJ_AHB_DIR_R = 0x00;
 	GPIO_PORTN_DIR_R = 0x03; //BIT0 | BIT1
 	GPIO_PORTF_AHB_DIR_R = 0x11;
+	GPIO_PORTQ_DIR_R = 0x0F;
+	GPIO_PORTP_DIR_R = 0x20;
 		
 	// 5. Limpar os bits AFSEL para 0 para selecionar GPIO sem funcao alternativa
 	GPIO_PORTA_AHB_AFSEL_R = 0x03;
 	GPIO_PORTJ_AHB_AFSEL_R = 0x00;
 	GPIO_PORTN_AFSEL_R = 0x00; 
 	GPIO_PORTF_AHB_AFSEL_R = 0x00;
+	GPIO_PORTQ_AFSEL_R = 0x00; 
+	GPIO_PORTP_AFSEL_R = 0x00;
 		
 	// 6. Setar os bits de DEN para habilitar I/O digital
 	GPIO_PORTA_AHB_DEN_R = 0x03;
 	GPIO_PORTJ_AHB_DEN_R = 0x03;   //Bit0 e bit1
 	GPIO_PORTN_DEN_R = 0x03; 		   //Bit0 e bit1
 	GPIO_PORTF_AHB_DEN_R = 0x11;   //Bit0 e bit1
+	GPIO_PORTQ_DEN_R = 0x0F;   //Bit0 e bit1
+	GPIO_PORTP_DEN_R = 0x20;   //PP5 como saida
 
 	
 	// 7. Habilitar resistor de pull-up interno, setar PUR para 1
@@ -116,6 +128,18 @@ extern void timerInit(void)
 }
 
 // -------------------------------------------------------------------------------
+uint32_t PortA_Input(valor)
+{
+	uint32_t temp;
+    //vamos zerar somente os bits menos significativos
+    //para uma escrita amigavel nos bits 0 e 1
+    temp = GPIO_PORTA_AHB_DATA_R & 0xFC;
+    //agora vamos fazer o OR com o valor recebido na funcao
+    temp = temp | valor;
+    GPIO_PORTA_AHB_DATA_R = temp; 
+}
+
+// -------------------------------------------------------------------------------
 // Funcao PortJ_Input
 // Le os valores de entrada do port J
 // Parametro de entrada: Nao tem
@@ -151,3 +175,26 @@ extern void PortF_Output(uint32_t valor)
     temp = temp | valor;
     GPIO_PORTF_AHB_DATA_R = temp; 
 }
+
+void PortQ_Output(uint32_t valor)
+{
+    uint32_t temp;
+    //vamos zerar somente os bits mais significativos
+    //para uma escrita amigável nos bits 0,1,2,3
+    temp = GPIO_PORTQ_DATA_R & 0xF0;
+    //agora vamos fazer o OR com o valor recebido na função
+    temp = temp | valor;
+    GPIO_PORTQ_DATA_R = temp; 
+}
+
+void PortP_Output(uint32_t valor)
+{
+    uint32_t temp;
+    //vamos zerar somente os bits mais significativos
+    //para uma escrita amigável nos bits 5
+    temp = GPIO_PORTP_DATA_R & 0xDF;
+    //agora vamos fazer o OR com o valor recebido na função
+    temp = temp | valor;
+    GPIO_PORTP_DATA_R = temp; 
+}
+
--- Lab03/Project/main.c
+++ Lab03/Project/main.c
@@ -21,12 +21,10 @@ typedef enum bool
    true
 } bool;
 
-uint32_t passo;
-uint32_t contPasso;
-uint32_t sysState;
-uint32_t readState;
-uint32_t motorPosition;
-uint32_t passo_completo[4] = {0x03, 0x06, 0x0C, 0x09};
+int32_t passo;
+int32_t contPasso;
+// complete step and middle step for a motor with 1.8° step angle and 2 phases	
+uint32_t passo_completo[4] = {0x01, 0x02, 0x04, 0x08};
 uint32_t meio_passo[8] = {0x01, 0x03, 0x02, 0x06, 0x04, 0x0C, 0x08, 0x09};
 unsigned char msg[50] = "Sentido: ,velocidade e posicionamento";
 
@@ -56,13 +54,19 @@ typedef struct States
    EN_ReadStates ucReadState;
 } States;
 
+typedef enum EN_MotorSpeed
+{
+  HALF_STEP,
+  FULL_STEP
+} EN_MotorSpeed;
+
 typedef struct MotorValues
 {
    unsigned char ucAngle[ANGLE_CHAR_SIZE];
    unsigned char ucDirection;
    unsigned char ucSpeed[SPEED_CHAR_SIZE];
-   unsigned short usAngleVal;
-   unsigned short usSpeedVal;
+   double usAngleVal;
+   EN_MotorSpeed usSpeedVal;
 } MotorValues;
 
 ///////// EXTERNAL FUNCTIONS INCLUSIONS //////////
@@ -78,6 +82,10 @@ void GPIO_Init(void);
 uint32_t PortJ_Input(void);
 void PortN_Output(uint32_t leds);
 void PortF_Output(uint32_t valor);
+void PortQ_Output(uint32_t valor);
+void PortA_Output(uint32_t valor);
+void PortP_Output(uint32_t valor);
+void acendeLED(uint16_t led_aceso);
 
 void motor_init(void);
 void PortH_Output(uint32_t data);
@@ -161,7 +169,7 @@ static void Pisca_leds(void);
 static States        stStates;
 static MotorValues   stMotorValues;
 static unsigned char ucIndex;
-static void motorRotation(char sentido, char velocidade);
+static void motorRotation(char sentido, EN_MotorSpeed velocidade);
 
 ///////// LOCAL FUNCTIONS IMPLEMENTATIONS //////////
 
@@ -175,12 +183,6 @@ int main(void)
    timerInit();
    initVars();
 
-	
-
-	while (1)
-	{
-		
-	}
    while (1)
    {
       switch (stStates.ucSysState)
@@ -193,7 +195,7 @@ int main(void)
 
          case SYS_ROTATE_MOTOR:
          {
-
+			Bobina();
          }
          break;
 
@@ -218,6 +220,379 @@ static void Pisca_leds(void)
 	SysTick_Wait1ms(250);
 	PortN_Output(0x1);
 	SysTick_Wait1ms(250);
+}
+
+static void initVars(void)
+{
+   uart_clearTerminal();
+
+   stStates.ucSysState  = SYS_READ_DATA;
+   stStates.ucReadState = READ_ANGLE;
+
+   stMotorValues.usAngleVal  = 0;
+   stMotorValues.usSpeedVal  = HALF_STEP;
+   stMotorValues.ucDirection = INVALID_NUMBER;
+
+   for (ucIndex = 0; ucIndex < ANGLE_CHAR_SIZE; ucIndex++)
+   {
+      stMotorValues.ucAngle[ucIndex] = 0;
+      stMotorValues.ucSpeed[ucIndex] = 0;
+   }
+
+   ucIndex = 0;
+
+   uart_uartTxString("Grau de giro: ", 14);
+
+   return;
+}
+
+
+static void readInput(void)
+{
+   switch (stStates.ucReadState)
+   {
+      case READ_ANGLE:
+      {
+         getAngleChar(&stMotorValues.ucAngle[0], &ucIndex);
+      }
+      break;
+
+      case READ_DIRECTION:
+      {
+         getDirectionChar(&stMotorValues.ucDirection);
+      }
+      break;
+
+      case READ_SPEED:
+      {
+         getSpeedChar(&stMotorValues.ucSpeed[0], &ucIndex);
+      }
+      break;
+
+      default:
+      {
+         // Should never get here
+      }
+      break;
+   }
+
+   return;
+}
+
+static void getAngleChar(unsigned char *pucAgleArray, unsigned char *pucIndex)
+{
+   if (*pucIndex < ANGLE_CHAR_SIZE)
+   {
+      unsigned char ucRxUartInt = uart_uartRxToInt();
+      
+      if (INVALID_NUMBER != ucRxUartInt)
+      {
+         pucAgleArray[*pucIndex] = ucRxUartInt;
+         (*pucIndex)++;
+         uart_uartTxIntToChar(ucRxUartInt);
+      }
+   }
+   else
+   {
+      waitForComma();
+   }
+
+   return;
+}
+
+static void getDirectionChar(unsigned char *pucDirection)
+{
+   if (INVALID_NUMBER == *pucDirection)
+   {
+      unsigned char ucRxUartInt = uart_uartRxToInt();
+
+      if (INVALID_NUMBER != ucRxUartInt)
+      {
+         *pucDirection = ucRxUartInt;
+         uart_uartTxIntToChar(ucRxUartInt);
+      }
+   }
+   else
+   {
+      waitForComma();
+   }
+
+   return;
+}
+
+static void getSpeedChar(unsigned char *pucSpeedArray, unsigned char *pucIndex)
+{
+   if (*pucIndex < SPEED_CHAR_SIZE)
+   {
+      unsigned char ucRxUartInt = uart_uartRxToInt();
+      
+      if (INVALID_NUMBER != ucRxUartInt)
+      {
+         pucSpeedArray[*pucIndex] = ucRxUartInt;
+         (*pucIndex)++;
+         uart_uartTxIntToChar(ucRxUartInt);
+      }
+   }
+   else
+   {
+      waitForComma();
+   }
+
+   return;
+}
+
+static void waitForComma(void)
+{
+   unsigned char ucRxUartChar = uart_uartRx();
+
+   if (',' == ucRxUartChar)
+   {
+      changeStatesAfterComma();
+   }
+
+   return;
+}
+
+static void changeStatesAfterComma(void)
+{
+   if (READ_SPEED == stStates.ucReadState)
+   {
+      bool bMustRotateMotor = checkMotorValues(&stMotorValues);
+      if (true == bMustRotateMotor)
+      {
+         uart_clearTerminal();
+         uart_uartTxString("Motor girando...", 16);
+         uart_uartTxString("\n\rPressione algum push_btn para abortar", 39);
+         stStates.ucSysState  = SYS_ROTATE_MOTOR;
+         stStates.ucReadState = READ_ANGLE;
+         ucIndex = 0;
+         TIMER2_CTL_R = 1; // Enables timer
+      }
+      else
+      {
+         initVars();
+      }
+   }
+   else // Goes to next read state
+   {
+      stStates.ucReadState = (stStates.ucReadState + 1) % 3;
+      ucIndex = 0;
+
+      switch (stStates.ucReadState)
+      {
+         case READ_DIRECTION:
+         {
+            uart_uartTxString("\n\rDirecao: ", 11);
+         }
+         break;
+
+         case READ_SPEED:
+         {
+            uart_uartTxString("\n\rVelocidade de giro: ", 22);
+         }
+         break;
+
+         default:
+         {
+            // Should never get here
+         }
+         break;
+      }
+   }
+
+   return;
+}
+
+static bool checkMotorValues(MotorValues *pstMotorValues)
+{
+   bool bMustRotateMotor = true;
+
+   pstMotorValues->usAngleVal = ((pstMotorValues->ucAngle[0] * 100) +
+                                 (pstMotorValues->ucAngle[1] * 10)  +
+                                 pstMotorValues->ucAngle[2]);
+   pstMotorValues->usSpeedVal = ((pstMotorValues->ucSpeed[0] * 100) +
+                                 (pstMotorValues->ucSpeed[1] * 10)  +
+                                 pstMotorValues->ucSpeed[2]);
+
+   if ((pstMotorValues->usAngleVal  > MAX_ANGLE) ||
+       (pstMotorValues->ucDirection > CLOCKWISE) ||
+       (pstMotorValues->usSpeedVal  > MAX_SPEED))
+   {
+      bMustRotateMotor = false;
+   }
+
+   return bMustRotateMotor;
+}
+
+static void waitForReset(void)
+{
+   unsigned char ucRxUartChar = uart_uartRx();
+
+   if ('*' == ucRxUartChar)
+   {
+      initVars();
+   }
+
+   return;
+}
+
+// Funcao: motorRotation
+// Descricao: movimenta o motor de passo mostrando no terminal o sentido, velocidade e em qual posicionamento o motor está se movendo, com resolução de 15°.
+// Parametros: angulo - angulo de rotacao
+//             sentido - sentido da rotacao (horario ou anti-horario)
+//             velocidade - velocidade da rotacao (completo ou meio passo)
+// Retorno: void
+void motorRotation(char sentido, EN_MotorSpeed velocidade)
+{
+
+	int contLed = 0;
+   double angulo_rotacionado = 0;
+   int auxVelocidade = 0;
+
+   if(velocidade == FULL_STEP){
+      auxVelocidade = 2;
+   }
+   else if(velocidade == HALF_STEP){
+      auxVelocidade = 1;
+   }
+
+
+	while(angulo_rotacionado < stMotorValues.usAngleVal){
+		SysTick_Wait1ms(2);
+
+		if(velocidade == FULL_STEP){
+			PortH_Output(passo_completo[passo]);
+		}
+		else if(velocidade == HALF_STEP){
+			PortH_Output(meio_passo[passo]);
+		}
+
+		if(stMotorValues.ucDirection == CLOCKWISE){
+			passo++;
+			if(passo == 4*auxVelocidade){
+				passo = 0;
+			}
+		}
+		else if(stMotorValues.ucDirection == COUNTERCLOCKWISE){
+			passo--;
+			if(passo == -1){
+				passo = 4*auxVelocidade - 1;
+			}
+		}
+
+		angulo_rotacionado = contPasso * 1.8;
+
+		if(ceil(angulo_rotacionado) %15 == 0){
+			msg[8] = sentido;
+			msg[10] = velocidade;
+			msg[12] = contPasso;
+			uart_uartTxString(msg, 50);
+		}
+
+		// Verifica se passou um intervalo de 45° para acender o LED
+		if(ceil(angulo_rotacionado) %45 == 0 && contLed < 8){
+			// Acende o LED correspondente
+         contLed++;
+			acendeLED(contLed);
+		}
+
+	}
+	stStates.ucSysState = SYS_WAIT_FOR_RESET;
+	return;
+}
+// Funcao: acendeLED
+// Descricao: acende um LED a cada 45° de rotacao e mantem acesos os LEDs anteriores
+// 			  também começa acendendo o LED da esquerda para a direita no sentido anti-horario e vice-versa para o sentido horario
+// Parametros: led_aceso - LED que sera aceso
+// Retorno: void
+void acendeLED(uint16_t led_aceso)
+{
+	switch(led_aceso)
+	{
+		case 0:
+			PortA_Output(0x00);
+			PortQ_Output(0x01);
+			break;
+		case 1:
+			PortA_Output(0x00);
+			PortQ_Output(0x02);
+			break;
+		case 2:
+			PortA_Output(0x00);
+			PortQ_Output(0x04);
+			break;		
+		case 3:
+			PortA_Output(0x00);
+			PortQ_Output(0x08);
+			break;		
+		case 4:
+			PortA_Output(0x10);
+			PortQ_Output(0x00);
+			break;		
+		case 5:
+			PortA_Output(0x20);
+			PortQ_Output(0x00);
+			break;		
+		case 6:
+			PortA_Output(0x40);
+			PortQ_Output(0x00);
+			break;		
+		case 7:
+			PortA_Output(0x80);
+			PortQ_Output(0x00);
+			break;		
+		default:
+			PortA_Output(0x00);
+			PortQ_Output(0x00);
+			break;
+	}
+	PortP_Output(0x20); //aciona transistor
+	return;
+}
+
+void Bobina()
+{
+   int contLed = 0, ledHor = 0, ledAnti = 7;
+   int auxVelocidade = 0;
+   int sentido = 0;
+   if(stMotorValues.usSpeedVal == FULL_STEP){
+      auxVelocidade = 2;
+   }
+   else if(stMotorValues.usSpeedVal == HALF_STEP){
+      auxVelocidade = 1;
+   }
+
+   if(stMotorValues.ucDirection == CLOCKWISE){
+      sentido = 1;
+   }
+   else if(stMotorValues.ucDirection == COUNTERCLOCKWISE){
+      sentido = -1;
+   }
+	while(contPasso < 4*auxVelocidade*stMotorValues.usAngleVal){
+		motorRotation(stMotorValues.ucDirection, stMotorValues.usSpeedVal);
+      if(contPasso%(15*auxVelocidade)==0){
+         msg[8] = stMotorValues.ucDirection;
+			msg[10] = stMotorValues.usSpeedVal;
+			msg[12] = contPasso;
+			uart_uartTxString(msg, 50);
+      }
+		if(contPasso%(45*auxVelocidade)==0)
+		{
+			if( contLed == 0 && sentido == -1){
+				 ledAnti--;
+             contLed = ledAnti;
+         }
+			else if ( contLed == 7 && sentido == 1){
+			    ledHor++;
+             contLed = ledHor;
+         }
+			else 
+				contLed += sentido;
+		}
+      
+		Liga_led(contLed);
+	}
+	Liga_led(10);
 }
 
 ///////// HANDLERS IMPLEMENTATIONS //////////
@@ -247,54 +622,5 @@ void Timer2A_Handler(void)
    GPIO_PORTN_DATA_R = GPIO_PORTN_DATA_R ^ 1;
 
    return;
-}
-
-void initVars(void)
-{
-	passo =0;
-	sysState = 0;
-	readState = 0;
-	motorPosition = 0;
-
-	return;
-}
-
-
-// Funcao: motorRotation
-// Descricao: movimenta o motor de passo mostrando no terminal o sentido, velocidade e em qual posicionamento o motor está se movendo, com resolução de 15°.
-// Parametros: angulo - angulo de rotacao
-//             sentido - sentido da rotacao (horario ou anti-horario)
-//             velocidade - velocidade da rotacao (completo ou meio passo)
-// Retorno: void
-void motorRotation(char sentido, char velocidade)
-{
-	// Atraso para estabilização do motor
-	SysTick_Wait1ms(2);
-
-	if(velocidade == '1'){
-		PortH_Output(meio_passo[passo]);
-	}
-	else{
-		PortH_Output(passo_completo[passo]);
-	}
-
-	if(sentido == 'H'){
-		passo++;
-		if(passo >= 8*velocidade){
-			passo = 0;
-		}
-	}
-	else{
-		passo--;
-		if(passo < 0){
-			passo = 8*velocidade - 1;
-		}
-	}
-
-	contPasso++;
-}
-
-void bobina(){
-	while()
 }
 
